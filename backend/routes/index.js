74650d08c0fbdec9c30ff85f0b233a5514eaa94d
Frangia22 <giacherofranc22@gmail.com>
2021-08-26 10:33:42 -0300

    Make two queries

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,45 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// Call sequelize
+const { sequelize, Op } = require('sequelize');
+// Import models
+const db = require('../models');
+/* GET home page last budgets */
+router.get('/', async (req, res) => {
+  const getLastBudgets = await db.personalBudget.findAll({
+    limit: 10,
+    order: [
+        ['date', 'DESC']
+    ]              
+    }).then(result => {
+      res.send(result);
+    });
 });
-
+// Get budget 
+//Get balance
+router.get('/balance', async (req, res) => { 
+  //SELECT SUM(amount) FROM `personalbudget` WHERE type = 'entry'; 
+  //Add up all income
+    const entry = await db.personalBudget.sum('amount', {
+        where: {
+            type: 'Entry'
+        }
+    })
+    .then(result => {
+        return(result);
+    })
+    //Add up all expenses
+    const egress = await db.personalBudget.sum('amount', {
+        where: {
+            type: 'Egress'
+        }
+    })
+    .then(result => {
+        return(result);
+    })
+    //difference between income and expense
+    const totalBalance = ((entry)-(egress));
+    console.log(totalBalance);
+    res.send({entry, egress, totalBalance})
+  });
 module.exports = router;
