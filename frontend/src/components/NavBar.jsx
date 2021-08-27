9ea146f077120bfdb7d57dcd6d884f0d7ccb102b
Frangia22 <giacherofranc22@gmail.com>
2021-08-26 22:29:01 -0300

    Nav list

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+
+export function NavBar() {
+        return (
+            <nav className="navbar">
+                <ul className="list">
+                    <li className="list-item"><a href="/.." className="list-item-link">Home</a></li>
+                    <li className="list-item"><a href="/AdminBudget" className="list-item-link">Admin budget</a></li>
+                    <li className="list-item"><a href="/AddBudget" className="list-item-link">Add Budget</a></li>
+                </ul>                
+            </nav>
+        )
+    }
+export default NavBar
