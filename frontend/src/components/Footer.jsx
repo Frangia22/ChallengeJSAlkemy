4d2bbbfc463f53910ed4308d0a599b0acc0e5671
Frangia22 <giacherofranc22@gmail.com>
2021-08-26 22:28:25 -0300

    Footer list

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+
+export function Footer() {
+    return (
+        <footer className="container">
+            <div className="footer">
+                <p>Derechos reservados 2021 &copy;</p>
+                <p>Desarrollado por <a className="link-port" href="http://frangiachero.netlify.app">FG-Fullstack</a></p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
