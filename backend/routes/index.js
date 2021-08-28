8dd741b4af147729c39b111b11c81b54dc5d9fde
Frangia22 <giacherofranc22@gmail.com>
2021-08-28 20:24:30 -0300

    Get budgets

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,7 +5,7 @@ const { sequelize, Op } = require('sequelize');
 // Import models
 const db = require('../models');
 /* GET home page last budgets */
-router.get('/', async (req, res) => {
+router.get('/home', async (req, res) => {
   const getLastBudgets = await db.personalBudget.findAll({
     limit: 10,
     order: [
@@ -42,4 +42,11 @@ router.get('/balance', async (req, res) => {
     console.log(totalBalance);
     res.send({entry, egress, totalBalance})
   });
+  //Get budgets
+  router.get('/getBudget',  (req, res) => {   
+    db.personalBudget.findAll()
+    .then(result => {
+        res.send(result);
+    });
+    });
 module.exports = router;
