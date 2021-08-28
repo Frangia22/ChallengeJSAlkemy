2dd2d4279dc74d083d2c87a684366b9ec47864db
Frangia22 <giacherofranc22@gmail.com>
2021-08-28 20:23:41 -0300

    Admin listo

---
+++ frontend/src/pages/AdminBudget.jsx
@@ -0,0 +1,81 @@
+import  React, { useState, useEffect } from 'react'
+import '../App.css'; 
+import Axios from 'axios';
+
+export function AdminBudget() {
+    //Fetch para traer los datos de la DB
+    const [ budgetList, setBudgetList ] = useState([]);
+    useEffect(() => {
+        Axios.get("/getBudget")
+        .then((response) => {
+            setBudgetList(response.data);
+        })
+        .catch(e=>console.log(e.response));
+    }, [])
+    //Fetch para eliminar un presupuesto
+    const deleteBudget = (id) =>{
+        Axios.get(`/deleteBudget/${id}`)
+        .then((result) => {
+            console.log(result);
+        })      
+    }
+    // Fetch para obtener el ID del presupuesto que quiero modificar
+    const editBudget = (id) =>{
+        Axios.get(`/editBudget/${id}`)
+        .then((result) => {
+            console.log('EL resultado es ', result);
+        })      
+    }
+    return (
+        <main className="principal" id="content">
+            <div className="container">
+                <div className="header-subtitle">
+                    <ul className="subtitle-list">
+                        <li className="subtitle-item subtitle-item-admin"><h1 className="subtitle-item-link">Administration panel</h1></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="container">
+                        <table className="table">
+                            <thead className="thead">
+                                <tr className="tr">
+                                    <th className="td hide-field">Operation number</th>
+                                    <th className="td hide-field">Concept</th>
+                                    <th className="td">$ Amount</th>
+                                    <th className="td hide-field">Date</th>
+                                    <th className="td">Type</th> 
+                                    <th className="td">Actions</th>                     
+                                </tr>
+                            </thead>
+                            { budgetList.map((val) => {
+                            return(
+                        <tbody className="tbody">
+                            <tr className="tr">
+                                <td className="td hide-field">{ val.id }</td>
+                                <td className="td hide-field">{ val.concept }</td>
+                                <td className="td">$ { val.amount }</td>
+                                <td className="td hide-field">{ val.date }</td>
+                                <td className="td">{ val.type }</td> 
+                                <td className="td actions">
+                                        <button className="btn btn-edit" onClick={
+                                            () => {
+                                                editBudget(val.id);
+                                                window.location.href = `/EditBudget/${val.id}`;
+                                            }
+                                        }>Edit</button>
+                                        <button className="btn btn-delete" type="submit" onClick={() => {deleteBudget(val.id); window.location.href = `/AdminBudget`;}} >Delete</button>
+                                    </td>                       
+                            </tr>
+                        </tbody>
+                            )
+                        }) }                           
+                        </table>          
+                    </div>
+                    <div className="add-product">
+                        <a className="btn btn-add" href="/AddBudget">Add operation</a>
+                    </div>
+                </main>
+    )
+}
+
+export default AdminBudget
