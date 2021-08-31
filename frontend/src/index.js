2304c49e06311bcb32a9965807e2b277b3538c00
Frangia22 <giacherofranc22@gmail.com>
2021-08-30 21:51:26 -0300

    Insert credentials

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Auth0Provider } from '@auth0/auth0-react'
 import App from './App';
 
 
 ReactDOM.render(
-    <App />,
+  <Auth0Provider domain="dev-mm5mo9no.us.auth0.com"
+      clientId="vgpksx4LQGHowCArGACARjCb5c1JLqki"
+      redirectUri={window.location.origin}>
+    <App />
+  </Auth0Provider>,
   document.getElementById('root')
 );
 
