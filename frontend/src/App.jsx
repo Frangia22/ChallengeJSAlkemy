7632f5501aa4267e58e1c4acb00c5e573005b9ae
Frangia22 <giacherofranc22@gmail.com>
2021-08-30 21:53:11 -0300

    App good

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,37 @@
+import React from 'react'
 import './App.css';
+import { BrowserRouter , Switch, Route } from 'react-router-dom';
+import { NavBar } from '../src/components/NavBar';
+import { Footer } from '../src/components/Footer';
+import { Home } from '../src/pages/Home';
+import { AdminBudget } from '../src/pages/AdminBudget';
+import { AddBudget } from '../src/pages/AddBudget.jsx';
+import { EditBudget } from '../src/pages/EditBudget';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Hello World</p>
-      </header>
+      <BrowserRouter>
+        <React.Fragment>
+          <NavBar />
+            <Switch> 
+              <Route path="/AdminBudget">
+                <AdminBudget />  
+              </Route>
+              <Route path="/AddBudget">
+                <AddBudget />  
+              </Route>
+              <Route path="/EditBudget/:id" >
+                <EditBudget /> 
+              </Route>                    
+              <Route path="/" exact={true}>
+                <Home />  
+              </Route>
+            </Switch>     
+          <Footer />   
+        </React.Fragment> 
+      </BrowserRouter>   
     </div>
   );
 }
